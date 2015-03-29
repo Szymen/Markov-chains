f996e21e5e0b26e338c676d7e331ae52afd415e7
szymon <szymmaslowski@gmail.com>
2015-03-29 23:02:58 +0200

    Hash function coded

--- hash.c
+++ hash.c
@@ -1,9 +1,17 @@
 
 
-int hash_val(char *slowa[], int ilosc_slow){
-	
-	
-	
-	
-	return wartosc;
+int hash_val(char *words[], int word_amount){
+
+	unsigned int MULTIPLIER=33;
+	unsigned int mod=10009;
+	unsigned int hash_val=0;	
+	for(i=0;i<word_amount;i++){
+		for(j=0;j<tab[i][j]!=;j++)
+		{
+			hash_val*=MULTIPLIER;
+			hash_val+=words[i][j]-64;	
+		}
+	}
+
+	return hash_val%mod;
 }
